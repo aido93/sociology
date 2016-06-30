49a1e3bafb474503780d16660a83b5a808862655
aido93 <aidos.tanatos@gmail.com>
2016-06-30 20:16:18 +0300

    Add files via upload

--- filter_death.pl
+++ filter_death.pl
@@ -14,7 +14,7 @@ my @all_str_m;
 while(<$file_m>)
 {
 	chomp;
-	~s/\r//g;
+	s/\r//g;
 	if (/^0,/)
 	{
 		push(@num_str_array_m, $num_str_m);
@@ -32,7 +32,7 @@ my @all_str_w;
 while(<$file_w>)
 {
 	chomp;
-	~s/\r//g;
+	s/\r//g;
 	if (/^0,/)
 	{
 		push(@num_str_array_w, $num_str_w);
@@ -46,30 +46,39 @@ close $file_w;
 foreach my $x (@num_str_array_m)
 {
 	$_=$all_str_m[$x-5];
-	s/.*[ \t\n\r]*$//;
-	s/ /\\ /;
+	s/([ ()])/\\\1/g;
 	s/,//;
-	my $b=join( '/' => ("regions_stat",  $_, "full", "man/"));
-	my $c=join( '/' => ("regions_stat",  $_, "city", "man/"));
-	my $d=join( '/' => ("regions_stat",  $_, "village", "man/"));
+	my $b=join( '/' => ("regions_stat",  $_, "full", "man"));
+	my $c=join( '/' => ("regions_stat",  $_, "city", "man"));
+	my $d=join( '/' => ("regions_stat",  $_, "village", "man"));
 	system( "mkdir -p $b");
 	system( "mkdir -p $c");
 	system( "mkdir -p $d");
-	say $b;
-	#print "$b/mortality.csv\n";
-	#print "$c/mortality.csv\n";
-	#print "$d/mortality.csv\n";
-	open my $full_out, '>', "$b/mortality.csv" or die "$b/mortality.csv\n";
-	open my $city_out, '>', "$c/mortality.csv" or die "$c/mortality.csv\n";
-	open my $village_out, '>', "$d/mortality.csv" or die "$d/mortality.csv\n";
-	print $full_out 	$all_str_m[0];
-	print $city_out 	$all_str_m[0];
-	print $village_out 	$all_str_m[0];
+    $b=~s/\\//g;
+    $c=~s/\\//g;
+    $d=~s/\\//g;
+    say $b;
+	open my $full_out, '>', "./$b/mortality.csv" or die "$!: $b/mortality.csv\n";
+	open my $city_out, '>', "./$c/mortality.csv" or die "$!: $c/mortality.csv\n";
+	open my $village_out, '>', "./$d/mortality.csv" or die "$!: $d/mortality.csv\n";
+	print $full_out 	$all_str_m[0]."\n";
+	print $city_out 	$all_str_m[0]."\n";
+	print $village_out 	$all_str_m[0]."\n";
+    my $t;
+    my $u;
 	for (my $i=0; $i<18;$i=$i+1)#18 категорий возрастов
 	{
-		print $full_out 	$all_str_m[$x+$i*4].substr($all_str_m[$x+$i*4+1],14);
-		print $city_out 	$all_str_m[$x+$i*4].substr($all_str_m[$x+$i*4+2],20);
-		print $village_out 	$all_str_m[$x+$i*4].substr($all_str_m[$x+$i*4+3],19);
+        $t=$all_str_m[$x+$i*4];
+        $t=~s/\.//;
+		$u=substr(@all_str_w[$x+$i*4+1],26);
+        $u=~s/"([0-9]*),([0-9]*)"/$1\.$2/g;
+		print $full_out 	$t.$u."\n";
+        $u=substr(@all_str_w[$x+$i*4+2],38);
+        $u=~s/"([0-9]*),([0-9]*)"/$1\.$2/g;
+		print $city_out 	$t.$u."\n";
+        $u=substr(@all_str_w[$x+$i*4+3],36);
+        $u=~s/"([0-9]*),([0-9]*)"/$1\.$2/g;
+		print $village_out 	$t.$u."\n";
 	}
 	close $full_out;
 	close $city_out;
@@ -78,27 +87,39 @@ foreach my $x (@num_str_array_m)
 foreach my $x (@num_str_array_w)
 {
 	$_=$all_str_m[$x-5];
-	s/.*[ \t\n\r]*$//;
-	s/ /\\ /;
+	s/([ ()])/\\\1/g;
 	s/,//;
-	my $b=join( '/' => ("regions_stat",  $_, "full", "woman/"));
-	my $c=join( '/' => ("regions_stat",  $_, "city", "woman/"));
-	my $d=join( '/' => ("regions_stat",  $_, "village", "woman/"));
+	my $b=join( '/' => ("regions_stat",  $_, "full", "woman"));
+	my $c=join( '/' => ("regions_stat",  $_, "city", "woman"));
+	my $d=join( '/' => ("regions_stat",  $_, "village", "woman"));
 	system( "mkdir -p $b");
 	system( "mkdir -p $c");
 	system( "mkdir -p $d");
+    $b=~s/\\//g;
+    $c=~s/\\//g;
+    $d=~s/\\//g;
 	#print "$b\n";
-	open my $full_out, '>', "regions_stat/$_/full/woman/mortality.csv" or die "$!: regions_stat/$_/full/woman/mortality.csv\n";
-	open my $city_out, '>', "regions_stat/$_/city/woman/mortality.csv" or die "$!: regions_stat/$_/full/woman/mortality.csv\n";
-	open my $village_out, '>', "regions_stat/$_/village/woman/mortality.csv" or die "$!: regions_stat/$_/full/woman/mortality.csv\n";
-	print $full_out 	$all_str_w[0];
-	print $city_out 	$all_str_w[0];
-	print $village_out 	$all_str_w[0];
+	open my $full_out, '>', "$b/mortality.csv" or die "$!: $b/mortality.csv\n";
+	open my $city_out, '>', "$c/mortality.csv" or die "$!: $c/mortality.csv\n";
+	open my $village_out, '>', "$d/mortality.csv" or die "$!: $d/mortality.csv\n";
+	print $full_out 	$all_str_w[0]."\n";
+	print $city_out 	$all_str_w[0]."\n";
+	print $village_out 	$all_str_w[0]."\n";
+    my $t;
+    my $u;
 	for (my $i=0; $i<18;$i=$i+1)#18 категорий возрастов
 	{
-		print $full_out 	@all_str_w[$x+$i*4].substr(@all_str_w[$x+$i*4+1],14);
-		print $city_out 	@all_str_w[$x+$i*4].substr(@all_str_w[$x+$i*4+2],20);
-		print $village_out 	@all_str_w[$x+$i*4].substr(@all_str_w[$x+$i*4+3],19);
+        $t=$all_str_m[$x+$i*4];
+        $t=~s/\.//;
+        $u=substr(@all_str_w[$x+$i*4+1],26);
+        $u=~s/"([0-9]*),([0-9]*)"/$1\.$2/g;
+		print $full_out 	$t.$u."\n";
+        $u=substr(@all_str_w[$x+$i*4+2],38);
+        $u=~s/"([0-9]*),([0-9]*)"/$1\.$2/g;
+		print $city_out 	$t.$u."\n";
+        $u=substr(@all_str_w[$x+$i*4+3],36);
+        $u=~s/"([0-9]*),([0-9]*)"/$1\.$2/g;
+		print $village_out 	$t.$u."\n";
 	}
 	close $full_out;
 	close $city_out;
